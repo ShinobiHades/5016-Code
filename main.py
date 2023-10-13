495ab527a09b322c032e5eb59a88159a6162424e
ShinobiHades <145733959+ShinobiHades@users.noreply.github.com>
2023-10-13 13:10:46 +1300

    main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from Ticket import *
+from Ticket import * # used to import all the functions from the ticket file
 
 
+# the main class to get user inputs and load the correct funtions in the ticket file
 def main():
     user_choice = ""
     while user_choice != "exit":
@@ -13,26 +14,27 @@ def main():
                             "Show <Statistics>.\n"
                             "> ")
         if user_choice.lower() == "create":
-            create()
+            create() # used to load up the ticket creation functions
         elif user_choice.lower() == "display":
             user_choice = input("Which ticket would you like to view? (Enter ticket number) \n> ")
             ticket = ticket_search(user_choice)
-            display(ticket)
+            display(ticket) # used to display a single ticket's data
         elif user_choice.lower() == "all":
-            display_all()
+            display_all() # used to display the ticket data for all created tickets
         elif user_choice.lower() == "respond":
             user_choice = input("Which ticket would you like to respond? (Enter ticket number) \n> ")
             ticket = ticket_search(user_choice)
-            respond(ticket)
+            respond(ticket) # used to load up the respond function to respond to a created ticket
         elif user_choice.lower() == "reopen":
             user_choice = input("Which ticket would you like to reopen? (Enter ticket number) \n> ")
             ticket = ticket_search(user_choice)
-            reopen(ticket)
+            reopen(ticket) # used to reopen a ticket
         elif user_choice.lower() == "statistics":
-            show_statistics()
+            show_statistics() # used to show the overall statistics for tickets, total number and how many tickets are open or closed
         else:
-            print("Please enter a valid option. :< \n")
+            print("Please enter a valid option. :< \n") # a check to make sure that a valid input is made
 
 
+# loads the function above making the entire program work
 main()
 print("Thank you for using this ticket system!")
